ea58044b664884e946d61353ab694ee818c4e9f2
jlangner87 <josh.langner@gmail.com>
2023-04-13 16:43:17 -0500

    add categories of modifications// next steps create component for each menu item and set router to load component for that drink. User selects menu item component by clicking tile image (this makes the process more user friendly by eliminating variables)

--- src/components/builder.jsx
+++ src/components/builder.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 function Builder() {
   let initialCalories = 0
   let initialState = {
+    size: 1,
     baseCalories: 0,
     milkCalories: 0
   }
@@ -16,23 +17,41 @@ function Builder() {
 
   const addCalories = (event) => {
     event.preventDefault()
-    let iteration = initialCalories + parseInt(formState.baseCalories) + parseInt(formState.milkCalories)
-    setTotalCalories(iteration)
+    let combinedCalories = initialCalories + parseInt(formState.baseCalories) + ( parseFloat(formState.size)* parseInt(formState.milkCalories))
+    setTotalCalories(Math.ceil(combinedCalories))
   }
-
 
   return (
     <div className="Builder">
       <h2>This is the builder</h2>
       <form onChange={handleChange}>
+      <label htmlFor="size">Size</label>
+        <select id="size">
+          <option value={1}>Choose One</option>
+          <option value={.75}>Small (12 oz)</option>
+          <option value={1}>Medium (16 oz)</option>
+          <option value={1.25}>Large (20 oz)</option>
+        </select><br/>
+        <label htmlFor="baseCalories">Drink</label>
         <select id="baseCalories">
           <option value={0}>Choose One</option>
-          <option value={500}>Latte</option>
-        </select>
+          <option value={0}>Latte</option>
+          <option value={0}>Espresso</option>
+          <option value={0}>Drip Coffee</option>
+          <option value={0}>Cold Brew</option>
+          <option value={0}>Chai Latte</option>
+          <option value={0}>Organic Tea</option>
+          <option value={0}>Lemonade</option>
+        
+        </select><br/>
+        <label htmlFor="milkCalories">Milk Type</label>
         <select id="milkCalories">
-          <option value={0}>Choose One</option>
-          <option value={50}>Whole</option>
-          <option value={75}>Breve</option>
+          <option value={0}>None</option>
+          <option value={195}>2%</option>
+          <option value={234}>Whole</option>
+          <option value={479}>Breve</option>
+          <option value={117}>Oat</option>
+          <option value={65}>Almond</option>
         </select>
       </form>
       <p>Calories: {totalCalories}</p>
