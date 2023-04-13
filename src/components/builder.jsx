95d21147f2a669ff2c6e095ddaf59740b3cbf7a4
jlangner87 <josh.langner@gmail.com>
2023-04-13 16:19:14 -0500

    add state for totalCalories

--- src/components/builder.jsx
+++ src/components/builder.jsx
@@ -1,19 +1,41 @@
 import { useState } from "react"
 
 function Builder() {
-  const initialState = 0
-  let [baseCalories, setBaseCalories] = useState(initialState)
-
-  const addCalories = (e) => {
-    e.preventDefault()
-    let iteration = baseCalories +=1
-    setBaseCalories(iteration)
+  let initialCalories = 0
+  let initialState = {
+    baseCalories: 0,
+    milkCalories: 0
   }
+  const [totalCalories, setTotalCalories]= useState(initialCalories)
+  const [formState, setFormState]= useState(initialState)
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+
+
+  const addCalories = (event) => {
+    event.preventDefault()
+    let iteration = initialCalories + parseInt(formState.baseCalories) + parseInt(formState.milkCalories)
+    setTotalCalories(iteration)
+  }
+
 
   return (
     <div className="Builder">
       <h2>This is the builder</h2>
-      <p>Calories: {baseCalories}</p>
+      <form onChange={handleChange}>
+        <select id="baseCalories">
+          <option value={0}>Choose One</option>
+          <option value={500}>Latte</option>
+        </select>
+        <select id="milkCalories">
+          <option value={0}>Choose One</option>
+          <option value={50}>Whole</option>
+          <option value={75}>Breve</option>
+        </select>
+      </form>
+      <p>Calories: {totalCalories}</p>
       <button onClick={addCalories}>Click</button>
     </div>
   )
